a58f5c0b39f5fa2f43cb4baf002088a5965ad6a0
Severiano Jaramillo <severiano.jaramillo@gmail.com>
2018-04-03 23:22:24 -0500

    Added resetGame function to reset status of the game

--- app/src/main/java/com/raywenderlich/timefigher/timefighter/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/timefigher/timefighter/MainActivity.kt
@@ -2,6 +2,7 @@ package com.raywenderlich.timefigher.timefighter
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.widget.Button
 import android.widget.TextView
 
@@ -11,7 +12,12 @@ class MainActivity : AppCompatActivity() {
     internal lateinit var gameScoreTextView: TextView
     internal lateinit var timeLeftTextView: TextView
 
+    internal lateinit var countDownTimer: CountDownTimer
+
     internal var score = 0
+    internal var gameStarted = false
+    internal val initialCountDown: Long = 60000
+    internal val countDownInterval: Long = 1000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,9 +27,30 @@ class MainActivity : AppCompatActivity() {
         gameScoreTextView = findViewById(R.id.game_score_text_view)
         timeLeftTextView = findViewById(R.id.time_left_text_view)
 
+        resetGame()
+
         tapMeButton.setOnClickListener { view ->
             incrementScore()
         }
+    }
+
+    private fun resetGame() {
+        score = 0
+        gameScoreTextView.text = getString(R.string.your_score, score.toString())
+        val initialTimeLeft = initialCountDown/ 1000
+        timeLeftTextView.text = getString(R.string.time_left, initialTimeLeft.toString())
+
+        countDownTimer = object: CountDownTimer(initialCountDown, countDownInterval) {
+            override fun onTick(millisUntilFinished: Long) {
+                val timeLeft = millisUntilFinished / 1000
+                timeLeftTextView.text = getString(R.string.time_left, timeLeft.toString())
+            }
+
+            override fun onFinish() {
+                // to be implemented later
+            }
+        }
+        gameStarted = false
     }
 
     private fun incrementScore() {
