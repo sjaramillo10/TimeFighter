b08ea64c32e3b275eadf92c3c555f99f4ed01d61
Severiano Jaramillo <severiano.jaramillo@gmail.com>
2018-04-03 23:08:36 -0500

    Increment the score on button click

--- app/src/main/java/com/raywenderlich/timefigher/timefighter/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/timefigher/timefighter/MainActivity.kt
@@ -2,11 +2,33 @@ package com.raywenderlich.timefigher.timefighter
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    internal lateinit var tapMeButton: Button
+    internal lateinit var gameScoreTextView: TextView
+    internal lateinit var timeLeftTextView: TextView
+
+    internal var score = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tapMeButton = findViewById(R.id.tap_me_button)
+        gameScoreTextView = findViewById(R.id.game_score_text_view)
+        timeLeftTextView = findViewById(R.id.time_left_text_view)
+
+        tapMeButton.setOnClickListener { view ->
+            incrementScore()
+        }
+    }
+
+    private fun incrementScore() {
+        score += 1
+        val newScore = getString(R.string.your_score, score.toString())
+        gameScoreTextView.text = newScore
     }
 }
